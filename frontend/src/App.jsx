19be82da9c0406a86be1e9394a6c7fdde1418da7
Vishal Verma <vermav999999@gmail.com>
2026-05-07 21:42:43 +0530

    Setup scalable frontend folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Dashboard from "./pages/Dashboard";
 import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        InterviewOS
-      </h1>
+    <div className="min-h-screen bg-black p-6">
+      <Dashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
